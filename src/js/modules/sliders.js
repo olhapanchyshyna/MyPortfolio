3f9b0028155f57a99bc358f8147900eba712600c
Olya <olapancisina1@gmail.com>
2023-06-30 15:39:50 +0200

    modile improve

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,24 +1,34 @@
 function sliders(){
-    let slideIndex = 1;
-    let offset = 0;
-
     function slider(slidesItem, sliderWrapperSelector, slidesFiledSelector, sliderSelector, dotSelector) {
         const slides = document.querySelectorAll(slidesItem);
         const sliderWrapper = document.querySelector(sliderWrapperSelector);
         const slidesFiled = document.querySelector(slidesFiledSelector);
         const slider = document.querySelector(sliderSelector);
         const dots = document.querySelectorAll(dotSelector);
+        let slideIndex = 1;
+        let offset = 0;
+        let touchStartX = 0;
+        let touchEndX = 0;
+        let swipeThreshold = 50;
        
         stepSlider();
       
         window.addEventListener('resize', () => {
             stepSlider();
+            handleResize();
         });
-      
+
+        function showDots(i = 0) {
+            dots.forEach(dot => {
+                dot.style.opacity = .5;
+            })
+            dots[i].style.opacity = 1;
+          }
+
         function stepSlider() {
-          const width = sliderWrapper.clientWidth + 'px';
+            const width = sliderWrapper.clientWidth + 'px';
       
-          if (window.innerWidth <= 576) {
+            if (window.innerWidth <= 576) {
                 slider.style.position = 'relative';
                 slidesFiled.style.display = 'flex';
                 slidesFiled.style.transition = '0.5s all';
@@ -30,6 +40,80 @@ function sliders(){
 
                 slidesFiled.style.width = 100 * slides.length + '%';
                 showDots();
+
+                function moveSlide (i) {
+                    const width = sliderWrapper.clientWidth + 'px';
+
+                    if(i == 1){
+                        if (offset == (parseInt(width) * (slides.length - 1))) {
+                            offset = 0;
+                        } else {
+                            offset += parseInt(width); 
+                        }
+            
+                        slidesFiled.style.transform = `translateX(-${offset}px)`;
+            
+                        if (slideIndex == slides.length) {
+                            slideIndex = 1;
+                        } else {
+                            slideIndex++;
+                        }
+                        
+                    }else if(i == -1){
+                        if (offset == 0) {
+                            offset = parseInt(width) * (slides.length - 1);
+                        } else {
+                            offset -= parseInt(width);
+                        }
+            
+                        slidesFiled.style.transform = `translateX(${-offset}px)`;
+            
+                        if (slideIndex == 1) {
+                            slideIndex = slides.length;
+                        } else {
+                            slideIndex--;
+                        }
+                        
+                    }
+
+                    dots.forEach(dot => {
+                        dot.style.opacity = '.5';
+                    });
+                    dots[slideIndex - 1].style.opacity = 1;
+                    
+                };
+
+               // Пороговое значение для определения свайпа
+
+                const handleTouchStart = (e) => touchStartX = e.touches[0].clientX;
+                const handleTouchMove = (e) => touchEndX = touchEndX = e.touches[0].clientX;;
+                
+
+                function handleTouchEnd(){
+                    let swipeLength = touchEndX - touchStartX;
+                    // Свайп вправо - переключить на предыдущий слайд
+                    if (swipeLength > swipeThreshold) moveSlide(-1);
+                    // Свайп влево - переключить на следующий слайд
+                    if (swipeLength < -swipeThreshold) moveSlide(1);
+
+                };
+
+
+                function handleResize(){
+                    let screenWidth = window.innerWidth;
+
+                    if (screenWidth > 577) {
+                        // Удалите обработчики событий
+                        sliderWrapper.removeEventListener('touchstart', handleTouchStart);
+                        sliderWrapper.removeEventListener('touchmove', handleTouchMove);
+                        sliderWrapper.removeEventListener('touchend', handleTouchEnd);
+                    } else {
+                        sliderWrapper.addEventListener('touchstart', handleTouchStart);
+                        sliderWrapper.addEventListener('touchmove', handleTouchMove);
+                        sliderWrapper.addEventListener('touchend', handleTouchEnd);
+                    }
+                }
+                handleResize();
 
                 dots.forEach(dot => {
                     dot.addEventListener('click', e => {
@@ -56,109 +140,12 @@ function sliders(){
                 slidesFiled.style.transform = '';
                 slidesFiled.style.transition = '';
                 sliderWrapper.style.overflow = '';
-          }
+            }
         }
       
-        function showDots(i = 0) {
-          dots[i].style.opacity = 1;
-        }
     }
     slider('.use__item', '.use__wrapper', '.use__list', '.use', '.use .dot');
-    slider('.portfolio__item', '.portfolio__wrapper', '.portfolio__list', '.portfolio', '.portfolio .dot');
-      
-    function touchSlider(slidesItem, sliderWrapperSelector, slidesFiledSelector, sliderSelector, dotSelector) {
-        const slides = document.querySelectorAll(slidesItem);
-        const sliderWrapper = document.querySelector(sliderWrapperSelector);
-        const slidesFiled = document.querySelector(slidesFiledSelector);
-        const slider = document.querySelector(sliderSelector);
-        const dots = document.querySelectorAll(dotSelector);
-
-
-        function moveSlide (i) {
-            const width = sliderWrapper.clientWidth + 'px';
-
-            if(i == 1){
-                if (offset == (parseInt(width) * (slides.length - 1))) { //650    px
-                    offset = 0;
-                } else {
-                    offset += parseInt(width); 
-                }
-    
-                slidesFiled.style.transform = `translateX(-${offset}px)`;
-    
-                if (slideIndex == slides.length) {
-                    slideIndex = 1;
-                } else {
-                    slideIndex++;
-                }
-                
-                
-            }else if(i == -1){
-                if (offset == 0) {
-                    offset = parseInt(width) * (slides.length - 1);
-                } else {
-                    offset -= parseInt(width);
-                }
-    
-                slidesFiled.style.transform = `translateX(${-offset}px)`;
-    
-                if (slideIndex == 1) {
-                    slideIndex = slides.length;
-                } else {
-                    slideIndex--;
-                }
-                
-            }
-
-            dots.forEach(dot => {
-                dot.style.opacity = '.5';
-            });
-            dots[slideIndex - 1].style.opacity = 1;
-            
-        };
-
-
-
-        let touchStartX = 0;
-        let touchEndX = 0;
-        let swipeThreshold = 50; // Пороговое значение для определения свайпа
-
-        const handleTouchStart = (e) => touchStartX = e.touches[0].clientX;
-        const handleTouchMove = (e) => touchEndX = touchEndX = e.touches[0].clientX;;
-        
-
-        function handleTouchEnd(){
-            let swipeLength = touchEndX - touchStartX;
-            // Свайп вправо - переключить на предыдущий слайд
-            if (swipeLength > swipeThreshold) moveSlide(-1);
-            // Свайп влево - переключить на следующий слайд
-            if (swipeLength < -swipeThreshold) moveSlide(1);
-
-        };
-
-
-        function handleResize(){
-
-            let screenWidth = window.innerWidth;
-
-            if (screenWidth > 577) {
-                // Удалите обработчики событий
-                sliderWrapper.removeEventListener('touchstart', handleTouchStart);
-                sliderWrapper.removeEventListener('touchmove', handleTouchMove);
-                sliderWrapper.removeEventListener('touchend', handleTouchEnd);
-            } else {
-                sliderWrapper.addEventListener('touchstart', handleTouchStart);
-                sliderWrapper.addEventListener('touchmove', handleTouchMove);
-                sliderWrapper.addEventListener('touchend', handleTouchEnd);
-            }
-        }
-        handleResize();
-        window.addEventListener('resize', handleResize);
-       
-    }
-    touchSlider('.use__item', '.use__wrapper', '.use__list', '.use', '.use .dot');
-    touchSlider('.portfolio__item', '.portfolio__wrapper', '.portfolio__list', '.portfolio', '.portfolio .dot');
-      
+    slider('.portfolio__item', '.portfolio__wrapper', '.portfolio__list', '.portfolio', '.portfolio .dot');      
 
 }
 export default sliders;
